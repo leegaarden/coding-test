784ae680c754c3585002960516dee8b9c93d9c65
leegaarden <dlwjddnjs081723@naver.com>
2025-06-11 15:42:36 +0900

    09 기타 제어문 강의 실습

---
+++ 09_기타제어문/class09-1.py
@@ -0,0 +1,17 @@
+# continue
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
+
+# break
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+
+# pass 
+for i in range(1, 11):
+    if i == 5:
+        pass
+    print(i)
